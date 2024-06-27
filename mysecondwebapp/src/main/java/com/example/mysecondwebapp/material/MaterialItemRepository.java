0471cad976d960620c808bb16694e87d36485534
thai6301 <thai632001@gmail.com>
2024-06-27 23:45:26 +0700

    commit

--- mysecondwebapp/src/main/java/com/example/mysecondwebapp/material/MaterialItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mysecondwebapp.material;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaterialItemRepository extends JpaRepository<MaterialItem, Integer> {
-
-}
--- mysecondwebapp/src/main/java/com/example/mysecondwebapp/material/RelMaterialCatRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mysecondwebapp.material;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RelMaterialCatRepository extends JpaRepository<RelMaterialCat, Integer> {
-
-}
